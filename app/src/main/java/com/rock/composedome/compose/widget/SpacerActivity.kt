2789f05026ac11cded817078986944f7a539bf47
llwdslal <466186888@qq.com>
2022-01-11 11:08:24 +0800

    添加 Spacer

---
+++ app/src/main/java/com/rock/composedome/compose/widget/SpacerActivity.kt
@@ -0,0 +1,10 @@
+package com.rock.composedome.compose.widget
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SpacerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
