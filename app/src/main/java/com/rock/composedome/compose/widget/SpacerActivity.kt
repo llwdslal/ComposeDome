e1320adff27def99aaf186347613dd0e0ca586ec
llwdslal <466186888@qq.com>
2022-01-13 15:09:20 +0800

    添加 Spacer

--- app/src/main/java/com/rock/composedome/compose/widget/SpacerActivity.kt
+++ app/src/main/java/com/rock/composedome/compose/widget/SpacerActivity.kt
@@ -2,9 +2,31 @@ package com.rock.composedome.compose.widget
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 class SpacerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContent {
+            SpacerBasic()
+        }
+    }
+}
+
+@Composable
+fun SpacerBasic(){
+    Row(
+        Modifier.fillMaxWidth().padding(8.dp).background(Color.Cyan, RoundedCornerShape(12.dp)).padding(6.dp)
+    ) {
+        Box(Modifier.background(Color.Red).size(100.dp, 80.dp))
+        Spacer(Modifier.width(20.dp))
+        Box(Modifier.background(Color.Magenta).height(60.dp).weight(1f))
     }
 }
