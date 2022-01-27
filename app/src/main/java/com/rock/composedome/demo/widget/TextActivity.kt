288a7d30a8bb1e4f9bd0ac5073e48e07f522583e
llwdslal <466186888@qq.com>
2022-01-27 11:27:52 +0800

    Text 补充

--- app/src/main/java/com/rock/composedome/demo/widget/TextActivity.kt
+++ app/src/main/java/com/rock/composedome/demo/widget/TextActivity.kt
@@ -9,8 +9,10 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.text.selection.SelectionContainer
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -20,6 +22,8 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.ParagraphStyle
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -42,6 +46,8 @@ class TextActivity : AppCompatActivity() {
                 TextSelectable()
                 Spacer(modifier = Modifier.height(8.dp))
                 TextAnnotated()
+                Spacer(modifier = Modifier.height(8.dp))
+                TextFontFamily()
             }
         }
     }
@@ -70,7 +76,7 @@ fun TextBasic(){
             Text(text = "TextBasic", textDecoration = TextDecoration.combine(listOf(TextDecoration.LineThrough,TextDecoration.Underline)))
         }
         //设置最大行数、文字溢出 、段落中的行距(单行不生效)
-        Text(text = "TextBasic".repeat(30), maxLines = 2, overflow = TextOverflow.Ellipsis, lineHeight = 50.sp)
+        Text(text = "TextBasic".repeat(30), maxLines = 2, overflow = TextOverflow.Ellipsis, lineHeight = 30.sp)
         //设置字符间距 ，lineHeight 单行不生效
         Text(text = "TextBasic", letterSpacing = 4.sp, lineHeight = 50.sp)
         //设置自动换行  默认 true , false 不换行
@@ -86,7 +92,7 @@ fun TextMultiStyle(){
         .fillMaxWidth()) {
         Text(text = buildAnnotatedString {
             //ParagraphStyle 设置段落样式
-            withStyle(style = ParagraphStyle(lineHeight = 50.sp)){
+            withStyle(style = ParagraphStyle(lineHeight = 30.sp)){
                 //SpanStyle 设置文本样式
                 withStyle(style = SpanStyle(color = Color.Red)){
                     append("Ann")
@@ -111,7 +117,7 @@ fun TextSelectable(){
         .padding(16.dp)
         .fillMaxWidth()) {
         SelectionContainer() {
-            Text(text = "TextSelectable".repeat(20))
+            Text(text = "TextSelectable".repeat(10))
         }
     }
 }
@@ -152,5 +158,44 @@ fun TextAnnotated(){
             }
         })
 
+    }
+}
+
+@Composable
+fun TextFontFamily(){
+    //配置字体
+    val customFamily = FontFamily(//瞎配的
+        Font(R.font.bickhamscriptfancy2, FontWeight.Thin), // Thin = W100
+        Font(R.font.biaukai, FontWeight.Normal),//Normal = W400
+        Font(R.font.belong_faith_regular_nrjjm, FontWeight.Normal, FontStyle.Italic),
+        Font(R.font.bmjua_regular, FontWeight.Medium),//Medium = FontWeight.W500
+        Font(R.font.din_alternate, FontWeight.Bold)//Bold = W700
+    )
+    Column(modifier = Modifier
+        .border(width = 1.dp, color = Color.Yellow, shape = RectangleShape)
+        .padding(16.dp)
+        .fillMaxWidth()
+        .verticalScroll(rememberScrollState())//滚动
+    ) {
+        //compose ui 包中的字体
+        Text(text = "FontFamily Default", fontFamily = FontFamily.Default)
+        Text(text = "FontFamily FontFamily", fontFamily = FontFamily.Serif)
+        Text(text = "FontFamily Monospace", fontFamily = FontFamily.Monospace)
+        Text(text = "FontFamily SansSerif", fontFamily = FontFamily.SansSerif)
+        Text(text = "FontFamily Cursive", fontFamily = FontFamily.Cursive)
+
+        //自定义字体  根据  fontWeight  fontStyle 自动寻找到对应的字体，
+        //没有配置的 weight ，会在配置中找到最近的 weight ，优先大的值。
+        Text(text = "CustomFontFamily", fontFamily = customFamily, fontStyle = FontStyle.Italic)
+        // Medium W500 , Bold = W700 ;
+        // W600 没配置过先向上找 +100  -> W700  -> din_alternate
+        Text(text = "CustomFontFamily W500", fontFamily = customFamily, fontWeight = FontWeight.Medium)
+        Text(text = "CustomFontFamily W700", fontFamily = customFamily, fontWeight = FontWeight.Bold)
+        Text(text = "CustomFontFamily W600", fontFamily = customFamily, fontWeight = FontWeight.W600)
+        Spacer(modifier = Modifier.height(8.dp))
+        // W200 没配置过先向上找 +100 -> W300 没找到,向下找 -> -100 -> W100 -> bickhamscriptfancy2
+        Text(text = "CustomFontFamily W100", fontFamily = customFamily, fontWeight = FontWeight.Thin)
+        Text(text = "CustomFontFamily W400", fontFamily = customFamily, fontWeight = FontWeight.Normal)
+        Text(text = "CustomFontFamily W200", fontFamily = customFamily, fontWeight = FontWeight.W200)
     }
 }
